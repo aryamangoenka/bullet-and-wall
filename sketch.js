3f178563838901943bb94422d6d3d8a7b69bcb60
aryamangoenka <73769393+aryamangoenka@users.noreply.github.com>
2020-11-29 16:32:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var bullet,speed,weight,wall,thickness;
+
+
+
+function setup() {
+  createCanvas(1600,400);
+  background(0)
+  thickness=random(22,83)
+  weight=random(30,52)
+  speed=random(223,321)
+  
+bullet=createSprite(150,200,80,20)
+bullet.velocityX=speed;
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor=color(80,80,80)
+}
+  function draw() {
+
+    
+    
+    drawSprites();
+    hasCollided();
+  }
+  
+  function hasCollided(bullet,wall){
+
+    bulletRightEdge=bullet.x +bullet.width;
+    wallLeftEdge=wall.x;
+    if(bulletRightEdge>=wallLeftEdge)
+    {
+      return true;
+    }
+return false;
+if(hasCollided(bullet,wall))
+    {
+      bullet.velocityX=0;
+      var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+
+      if(damage>10)
+      {
+        wall.shapeColor=color(255,0,0);
+      }
+
+
+      if(damage<10)
+        {
+          wall.shapeColor=color(0,255,0);
+        }
+  }
+}
